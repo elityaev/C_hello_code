238479be4e1c7349aa70d14286807f1dd9a2886b
elit <elityaev@yandex.ru>
2022-11-22 12:47:45 +0300

    Add seminar 2 + dz

--- seminar1/sem1_dz/Task4/Program.cs
+++ seminar1/sem1_dz/Task4/Program.cs
@@ -1,7 +1,8 @@
 ﻿Console.WriteLine("Введите число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 int i = 2;
-while (i <= numberA){
+while (i <= numberA)
+{
     if (i % 2 == 0){
         Console.Write($"{i}, ");
     } 
